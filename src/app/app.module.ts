a3876d6ddb62b2ca019ebff88399e0442f97db21
Lagnesh Thakur <lagneshthakur@gmail.com>
2018-02-24 16:08:27 +0530

    PWA setup manifest and sw, using sw-precache-webpack-plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    TopnavComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([{
+      path : '' , 
+      component : HomeComponent
+    }, 
+    {
+      path : 'home' , 
+      component : HomeComponent
+    }, 
+    {
+      path : 'projects' , 
+      component : ProjectsComponent
+    },
+    {
+      path : 'about' , 
+      component : AboutComponent
+    },
+    {
+      path : 'contact' , 
+      component : ContactComponent
+    }
+  ])
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
